4ca7cf41bb4751c0e72c9185a2ec3ad52451cd35
Dane Jensen <dane@rightscale.com>
2011-09-02 17:38:32 -0700

    add read_many to do a multi-get. better support using columns

--- lib/adapter/cassandra.rb
+++ lib/adapter/cassandra.rb
@@ -10,6 +10,12 @@ module Adapter
     def read(key)
       decode(client.get(options[:column_family], key_for(key)))
     end
+    
+    def read_many(keys)
+      keys = keys.collect { |k| key_for(k)}
+      rows = client.multi_get(options[:column_family], keys)
+      rows.inject({}) { |result, (k,v)| result.update(k => decode(v)) }
+    end
 
     def write(key, value)
       client.insert(options[:column_family], key_for(key), encode(value))
@@ -18,29 +24,31 @@ module Adapter
     def delete(key)
       read(key).tap { client.remove(options[:column_family], key_for(key)) }
     end
-
+    
     def clear
       client.clear_keyspace!
     end
 
     def encode(value)
-      case value
-      when String
-        {"toystore" => value}
-      when Hash
-        value.inject({}) { |result, (k, v)| result.update(k.to_s => v.to_s) }
+      value.inject({}) do |result, (k, v)| 
+        case v
+        when String
+          result.update(k.to_s => v)
+        when NilClass # skip nil values. toystore sends them, but cassandra hates 'em.
+          result
+        else
+          result.update(k.to_s => v) # right now we're expecting to get a properly-serialized value...
+        end
       end
     end
 
     def decode(value)
       return nil if value.empty?
-      case value
-      when Hash
-        value.has_key?('toystore') ? value['toystore'] : value
-      else
-        value
-      end
+      # NOTE: this is super critical here, things break in HORRIBLY MYSTERIOUS ways if we try to
+      # pass back an OrderedHash. not really sure why yet.
+      value.inject({}) { |result, (k, v)| result.update(k => v) }
     end
+
   end
 end
 
